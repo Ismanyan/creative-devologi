1e6f94fad4713c227ddd66cde3d617601db68c7a
Bilkis Ismail <bilkisismail07@Gmail.com>
2019-07-25 20:50:56 +0700

    Desktop Stable v.1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Creative - Start Bootstrap Theme</title>
+
+  <!-- Font Awesome Icons -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/animate.css">
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="css/creative.css" rel="stylesheet">
+  <link href="css/testimonial.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 animated fadeInDown" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <img src="img/Creative Devologi New Logo.png" alt="Creative Devologi" width="120">
+      </a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#page-top">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">SERVICES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">PORTFOLIO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#testimonial">TESTIMONY</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead animated fadeIn slow">
+    <div class="h-100">
+      <div class="row h-100 w-100">
+        <div class="col-lg-6 align-self-end text-center animated fadeInLeft">
+          <h1 class="text-uppercase text-white font-weight-bold" style="font-size:65px;">CREATIVE<br> DEVOLOGI</h1>
+          <p class="text-white" style="font-family: 'Montserrat', sans-serif; font-size:30px;">Hallo, Welcome in <br> our Website, Thanks</p>
+          <a href="#" class="btn btn-primary btn-lg" id="order-btn"><b>ORDER NOW</b></a>
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- About Section -->
+  <section class="page-section" id="about">
+    <div class="row about-row h-100 w-100">
+      <div class="col-md-7">
+        <img src="img/about us icon.png" class="about-img animated fadeInLeft slow" alt="about us">
+      </div>
+      <div class="col-md-5 text-left about-desc animated fadeIn slow">
+        <h1 class="text-uppercase font-weight-bold title-about">ABOUT US</h1>
+        <p class="content-about">Kreatologi adalah Jasa Penyedia pembuatan
+          Website, Aplikasi android & Ios, Design dan
+          Production House. Memiliki kualitas yang sudah di
+          aksi serta team yang sudan berpengalaman untuk
+          memberikan hasil yang memuaskan untuk anda.</p>
+        <h1 class="text-uppercase font-weight-bold sub-title-about">VALUES PLUS</h1>
+        <div class="row text-center">
+          <div class="col-md-2 about-icon">
+            <img class="w-100" src="img/icon/professional service.png" alt="professional service">
+            <p><b>Professional Services</b></p>
+          </div>
+          <div class="col-md-2 about-icon">
+            <img class="w-100" src="img/icon/fast and experience team.png" alt="fast and experience team">
+            <p><b>Fast and Experienced Team</b></p>
+          </div>
+          <div class="col-md-2 about-icon">
+            <img class="w-100" src="img/icon/100% quality guarantee.png" alt="100% quality guarantee">
+            <p><b>100% Quality Guarantee</b></p>
+          </div>
+          <div class="col-md-2 about-icon">
+            <img class="w-100" src="img/icon/premium design.png" alt="premium design">
+            <p><b>Premium <br> Design</b></p>
+          </div>
+          <div class="col-md-2 about-icon">
+            <img class="w-100" src="img/icon/cost affective.png" alt="cost affective">
+            <p><b>Cost <br> Affective</b></p>
+          </div>
+          <div class="col-md-2 about-icon">
+            <img class="w-100" src="img/icon/secure and safe.png" alt="secure and safe">
+            <p><b>Secure <br> and Save</b></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Services Section -->
+  <section class="page-section" id="services">
+    <div class="container">
+      <h2 class="text-center services-title text-uppercase text-white font-weight-bold mb-5 animated fadeIn slow" style="font-size:65px;">SERVICES AND PORTFOLIO</h2>
+      <div class="row text-center animated fadeInLeft slow">
+        <div class="col-md-4">
+          <div class="card services-card mt-2">
+            <img class="w-100" src="img/services/professional website.png" alt="professional website">
+            <h4 class="mt-5 services-card-title">PROFESSIONAL WEBSITE</h4>
+            <p class="services-card-desc">Company Profile, Product Profile, E-Invitation, landing page, Event information, Etc.</p>
+            <div class="row mt-3">
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">PORTFOLIO</a>
+              </div>
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">ORDER NOW</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card services-card mt-2">
+            <img class="w-100" src="img/services/graphic design.png" alt="graphic design">
+            <h4 class="mt-4 services-card-title">GRAPHIC DESIGN</h4>
+            <p class="services-card-desc">Corporate Identity ( Map, ID card, Logo etc), Event Kit Design, Modern Invitation, Poster, Flyer, Social Media Post (Instagram), Etc.</p>
+            <div class="row">
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">PORTFOLIO</a>
+              </div>
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">ORDER NOW</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card services-card mt-2">
+            <img class="w-100" src="img/services/video shoot.png" alt="video shoot">
+            <h4 class="mt-4 services-card-title">VIDEO SHOOT</h4>
+            <p class="services-card-desc">Event, Documentation, Product Video, Wedding Video, Etc.</p>
+            <div class="row mt-5">
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">PORTFOLIO</a>
+              </div>
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">ORDER NOW</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row text-center animated fadeInRight slow mt-5">
+        <div class="col-md-4">
+          <div class="card services-card mt-2">
+            <img class="w-100" src="img/services/editing.png" alt="editing">
+            <h4 class="mt-2 services-card-title">EDITING</h4>
+            <p class="services-card-desc">Event Video, Wedding Video, Animation 2D Invitation, Motion Graphic, Family Video, Vlog or Touring Video, Youtube Content Video, Etc.</p>
+            <div class="row mt-2">
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">PORTFOLIO</a>
+              </div>
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">ORDER NOW</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card services-card mt-2">
+            <img class="w-100" src="img/services/photography.png" alt="photography">
+            <h4 class="mt-1 services-card-title">PHOTOGRAPHY</h4>
+            <p class="services-card-desc">Wedding Photo, Event Photo, Documentation, Photo Product, Etc.</p>
+            <div class="row mt-4">
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">PORTFOLIO</a>
+              </div>
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">ORDER NOW</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card services-card mt-2">
+            <img class="w-100 mb-5" src="img/services/android ios dev.png" alt="android ios dev" style="width:300px !important;">
+            <h4 class="mt-4 services-card-title">ANDROID & IOS DEVELOPMENT</h4>
+            <p class="services-card-desc">Android Application, IOS Application, Etc.</p>
+            <div class="row mt-3">
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">PORTFOLIO</a>
+              </div>
+              <div class="col-md-6">
+                <a href="" class="btn btn-primary mt-2 w-100 services-card-btn">ORDER NOW</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Testimony Section -->
+  <section class="page-section" id="testimonial">
+    <div class="container">
+      <h1 class="text-uppercase font-weight-bold title-testimony animated fadeIn slow mb-5">TESTIMONY</h1>
+      <div id="myCarousel" class="carousel slide animated fadeInLeft slow" data-ride="carousel">
+        <!-- Wrapper for carousel items -->
+        <div class="carousel-inner">
+          <div class="item carousel-item active">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="card card-carousel p-5">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <div class="img-box"><img src="img/testimoni/1.jpg" alt="bilkis"></div>
+                    </div>
+                    <div class="col-md-8">
+                      <p class="testimonial">Wah, bagus sekali pelayanannya, saya sangat puas bekerjasama dengan anda, Terimkasih!</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="card card-carousel p-5">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <div class="img-box"><img src="img/testimoni/1.jpg" alt="bilkis"></div>
+                    </div>
+                    <div class="col-md-8">
+                      <p class="testimonial">Benar-benar sangat Recommended, Hasil kerja yang Professional, dan sangat Bertanggung Jawab, Terimkasih!</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Carousel controls -->
+        <a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+          <i class="fa fa-angle-left"></i>
+        </a>
+        <a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+          <i class="fa fa-angle-right"></i>
+        </a>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- partner section -->
+  <section class="partner" id="partner">
+    <div class="container">
+      <h1 class="text-uppercase font-weight-bold partner-title animated fadeIn slow">PARTNER, AND CLIENT</h1>
+      <br>
+    </div>
+  </section>
+
+
+  <!-- Contact Section -->
+  <section class="contact page-section" id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 animated fadeInLeft slow">
+          <img src="img/icon contact us.png" alt="icon contact us" class="w-100">
+        </div>
+        <div class="col-md-6 animated fadeIn slow">
+          <h1 class="text-uppercase font-weight-bold contact-title mb-5">CONTACT US</h1>
+          <div class="row">
+            <div class="col-md-3">
+              <img src="img/icon/address.png" alt="address" width="120">
+            </div>
+            <div class="col-md-9">
+              <p class="text-left mt-4 contact-desc">Bukit cimanggu city, Greenland, Cluster River Park View Blok CC2/10, Cibadak, Tanah Sereal, Kota Bogor </p>
+            </div>
+            <div class="col-md-3">
+              <center>
+                <img src="img/icon/whatsapp.png" alt="whatsapp" width="80" class="">
+              </center>
+            </div>
+            <div class="col-md-9">
+              <a href="tel:[085810840409]" class="d-block text-left mt-4 contact-desc">085810840409 </a>
+            </div>
+            <div class="col-md-3">
+              <center>
+                <img src="img/icon/email.png" alt="email" width="80" class="mt-4">
+              </center>
+            </div>
+            <div class="col-md-9">
+              <a href="mailto:creativedevologi@gmail.com" class="d-block text-left mt-4 contact-desc">creativedevologi@gmail.com</a>
+              <a href="mailto:info@creativedevologi.com" class="d-block text-left mt-1 contact-desc">info@creativedevologi.com</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5" id="footer">
+    <div class="container">
+      <a href="#page-top">
+        <img class="float-right" src="img/back to top.png" alt="top" width="50" style="margin-top:-30px;">
+      </a>
+      <div class="text-center footer-desc">Copyright Creative Devologi 2019</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
